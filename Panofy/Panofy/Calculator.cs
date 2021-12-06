d352e863131b0c4150a9ef9322386cfc3ddc80e4
fer-dev-mx <62954322+fer-dev-mx@users.noreply.github.com>
2021-12-06 10:11:41 -0600

    Add class Calculator

---
+++ Panofy/Panofy/Calculator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Panofy
+{
+    public class Calculator
+    {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public int Subtract(int x, int y)
+        {
+            return x - y;
+        }
+    }
+}
